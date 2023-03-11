0fda25abc77d898bdde210298c793f7ef1c38a82
Matthew <colonialp9@gmail.com>
2023-03-11 10:51:57 +0200

    Added custom.el for in-app settings

---
+++ custom.el
@@ -0,0 +1,20 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("1a1ac598737d0fcdc4dfab3af3d6f46ab2d5048b8e72bc22f50271fd6d393a00" "e3daa8f18440301f3e54f2093fe15f4fe951986a8628e98dcd781efbec7a46f2" "7a424478cb77a96af2c0f50cfb4e2a88647b3ccca225f8c650ed45b7f50d9525" "636b135e4b7c86ac41375da39ade929e2bd6439de8901f53f88fde7dd5ac3561" "944d52450c57b7cbba08f9b3d08095eb7a5541b0ecfb3a0a9ecd4a18f3c28948" default))
+ '(helm-minibuffer-history-key "M-p")
+ '(linum-format 'dynamic)
+ '(python-indent-guess-indent-offset nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :foundry "CTDB" :family "Hack Nerd Font"))))
+ '(centaur-tabs-selected-modified ((t (:background "#002b36" :foreground "#93a1a1"))))
+ '(centaur-tabs-unselected-modified ((t (:background "#00212B" :foreground "#93a1a1"))))
+ '(linum ((t (:inherit default :foreground "#56697A" :slant italic :weight normal))))
+ '(window-divider ((t (:inherit vertical-border :foreground "#00232B")))))
